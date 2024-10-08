e6fc06c5ef1a665a46fa1c4c2d7521cedef91251
silviabielikova <sisa@bielik.sk>
2024-10-08 11:33:37 +0200

    added types and constants

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface User {
+    id: string;
+    name: string;
+    gender: string;
+    banned: boolean;
+}
+
+export interface Animal {
+    id: string;
+    name: string;
+    type: string;
+    age: number;
+}
